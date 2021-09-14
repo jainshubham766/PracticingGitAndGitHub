459b594c2550c727c156166dc420397413433adb
Shubham Jain <jainshubham766gmail.com>
2021-09-14 16:30:21 +0530

    adding NewFile

---
+++ NewFile.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class NewFile2
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+                System.out.println("HelloWorld);
+    }
+}
